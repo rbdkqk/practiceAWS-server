494b6f1659e6cde0d865d936a9aab0ab2486f1b6
rbdkqk <47794257+rbdkqk@users.noreply.github.com>
2020-09-25 00:43:33 +0900

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,27 +1,27 @@
-module.exports = {
-  apps: [
-    {
-      script: "./app.js",
-      env: {
-        DATABASE_PASSWORD: 123,
-      },
-      env_production: {
-        DATABASE_PASSWORD: 123,
-      },
-    },
-  ],
+// module.exports = {
+//   apps: [
+//     {
+//       script: "./app.js",
+//       env: {
+//         DATABASE_PASSWORD: 123,
+//       },
+//       env_production: {
+//         DATABASE_PASSWORD: 123,
+//       },
+//     },
+//   ],
 
-  // deploy: {
-  //   production: {
-  //     user: "SSH_USERNAME",
-  //     host: "SSH_HOSTMACHINE",
-  //     ref: "origin/master",
-  //     repo: "GIT_REPOSITORY",
-  //     path: "DESTINATION_PATH",
-  //     "pre-deploy-local": "",
-  //     "post-deploy":
-  //       "npm install && pm2 reload ecosystem.config.js --env production",
-  //     "pre-setup": "",
-  //   },
-  // },
-};
+//   // deploy: {
+//   //   production: {
+//   //     user: "SSH_USERNAME",
+//   //     host: "SSH_HOSTMACHINE",
+//   //     ref: "origin/master",
+//   //     repo: "GIT_REPOSITORY",
+//   //     path: "DESTINATION_PATH",
+//   //     "pre-deploy-local": "",
+//   //     "post-deploy":
+//   //       "npm install && pm2 reload ecosystem.config.js --env production",
+//   //     "pre-setup": "",
+//   //   },
+//   // },
+// };
