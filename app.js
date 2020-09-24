bec217ea36f69a6d307302f6b95144c926dc00c9
rbdkqk <rbdkqk@gmail.com>
2020-09-24 23:02:03 +0900

    try cors

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ let whitelist = [
 const corsOptions = {
   origin: (origin, callback) => {
     console.log(`origin`, origin);
-    if (whitelist.indexOf(origin) !== -1) callback(null, true);
+    if (!origin || whitelist.indexOf(origin) !== -1) callback(null, true);
     else callback(new Error("Not allowed by CORS"));
   },
   credentials: true,
